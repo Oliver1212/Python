1093e3808b4ea0f72dc7426f99c8b5d932251142
Horváth Olivér <horvatho20@ADY-NAGYATAD.SULINET.HU>
2022-03-23 09:39:53 +0100

    5. feladat

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -6,7 +6,7 @@ f.close()
 print("2. feladat: " + str(len(adatok)) + "db")
 
 print("3. feladat: "+str(sum(int(e.split(";")[1]) + int(e.split(";")[2]) for e in adatok)) + " fő")
-
+ossz=0
 for e in adatok:
       temp=e.split(";")
       tulelo=int(temp[1])
@@ -18,4 +18,53 @@ print("3. feladat: "+str(ossz) + " fő")
 
 kulcsszo=input("4.feladat: kulcsszó: ")
 van=False
+
 #find()
+for e in adatok:
+      if e.find(kulcsszo)>=0:
+            van=True
+            break
+if van:
+      print("Van találat")
+else:
+      print("Nincs találat")
+
+adatok2=[]
+for e in adatok:
+      temp=e.split(";")
+      temp[1]=int(temp[1])
+      temp[2]=int(temp[2])
+      adatok2.append(temp)
+print(adatok2)
+
+talalat=[e for e in adatok if e.find(kulcsszo)>=-1]
+#print(talalat)
+
+if len(talalat)>0:
+      print("Van találat")
+else:
+      print("Nincs találat")
+
+#gyerekek-masodosztaly;24;0
+
+
+print("5. feladat")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
