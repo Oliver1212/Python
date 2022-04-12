c1a75d63cfff44e9148325564771b3e61be215cf
Oliver1212 <horvath.oliver.20@ady-nagyatad.hu>
2022-04-12 08:42:37 +0200

    .

--- OOP1.py
+++ OOP1.py
@@ -1,9 +1,31 @@
+#osztály definíció
+
 class kutya:
+    #konstruktor
     def __init__(self,nev):
         self.nev=nev
+    #osztály függvény
+    def ugat(self):
+        print("vau-vau(" + self.nev+")")
+#példányosítás
+k=kutya("Armageddon")
+k.ugat()
+print(k.nev)
+#osztályváltozó értékadása
+k.nev="Bruno"
+print(k.nev)
+#Öröklés,leszármasztatás
+class NemetJuhasz(kutya):
+    #új függvény
+    def pitiz(self):
+        print(self.nev + ": Nein!!!")
+    #függvény felűlírás
+    def ugat(self):
+        print("wau-wau")
+        
+n=NemetJuhasz("Rex")
+n.ugat()
+n.pitiz()
 
-    def ugat():
-        print("vau-vau(" + self.nev+".")
-
-egyKutya=kutya("Armageddon")
-print(egyKutya.nev)
+n2=NemetJuhasz("Kondér")
+n2.pitiz()
