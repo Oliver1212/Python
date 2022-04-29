64fdc3ff726a1306526314c87f7b4342781b832a
Horváth Olivér <horvatho20@ADY-NAGYATAD.SULINET.HU>
2022-04-29 11:28:01 +0200

    .

---
+++ Szovegformazas.py
@@ -0,0 +1,16 @@
+f1="ide jön a szám {}"
+
+print(f1.format("kettő"))
+print(f1.format(2))
+
+szam=input("Kérek egy számot: ")
+print(f1.format(szam))
+
+f2="ide is jön valami {1}, ide meg más {0}"
+print(f2.format(123,323))
+
+f3="My name is (surname), (firstname) (surname)."
+print(f3.format(firstname="James",surname="Bond"))
+
+f4="Az én nevem (surname), (surname) (firstname)."
+print(f4.format(firstname="James",surname="Bond"))
